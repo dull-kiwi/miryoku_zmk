f0e73672d16fc21b4d7eb1aade22ec74233e48a8
dull-kiwi <lakb2f1h@anonaddy.com>
2025-09-25 08:44:26 -0400

    Change nav layer define in custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,7 +16,7 @@ U_NP,              U_NP,              &kp LALT,          &kp SPC,           &mo 
 &kp LCTRL,         &kp NUM_3,         &kp X,             &kp NUM_4,         &kp NUM_7,          &kp BSLH,          &kp F1,            &kp F2,            &kp F3,            &kp GRAVE,        \
 U_NP,              U_NP,              &to U_BASE,        &kp SPC,           &none,              &kp MINUS,         &kp NUM_0,         &kp DOT,           U_NP,              U_NP
 
-#define MIRYOKU_ALTERNATIVES_NAV \
+#define MIRYOKU_LAYER_NAV \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,            &u_caps_word,         \
 U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
